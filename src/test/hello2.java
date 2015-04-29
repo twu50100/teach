393b5de55c21e38f0bedf023c154725d41b1659e
michelle <michelle@10.0.2.11>
2015-04-29 21:30:31 +0800

    hw

--- src/test/hello2.java
+++ src/test/hello2.java
@@ -9,7 +9,7 @@ public class hello2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("春曉");
-		System.out.println("春眠不覺曉，");
+		System.out.println("春眠不覺曉test，");
 		System.out.println("處處聞啼鳥;");
 		System.out.println("夜來風雨聲，");
 		System.out.println("花落知多少。");
