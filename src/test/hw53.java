99c1d9daeb38620137f1f712e4689e3565ac114c
michelle <michelle@michelletekimbp>
2015-05-08 01:25:12 +0800

    99

--- src/test/hw53.java
+++ src/test/hw53.java
@@ -5,6 +5,15 @@ import java.util.Arrays;
 public class hw53 {
 
 	public static void main(String[] args) {
+		int[] StrTochar=args[0].toCharArray();
+		for(int i=0;i<args.length;i++){
+			StrTochar[1]=toint(args[i]);
+		}
 	
+	}
+
+	private static int toint(String string) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
