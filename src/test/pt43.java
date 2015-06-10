7085d32d413636dbc12ce52e5eb96f91afe507a1
hrne <hrne@hrne-PC>
2015-06-10 22:51:05 +0800

    f

--- src/test/pt43.java
+++ src/test/pt43.java
@@ -8,7 +8,7 @@ public class pt43 {
 		// TODO Auto-generated method stub
 		int pass = 0;
 		int fail;
-		int []math ={50,65,80,48,35,93,57,90,86,77};
+		int []math ={50,65,80,48,35,93,57,90,86,76};
 		Arrays.sort(math);
 		for(int i=math.length;i>=1; i--){
 			System.out.print(""+pass);
