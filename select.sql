20a8533b035a180a72d048f7c510a41ee4997429
twu50100 <twu50100@gmail.com>
2018-04-09 13:38:42 +0800

    Add files via upload

---
+++ select.sql
@@ -0,0 +1,77 @@
+--alter session set current_schema=expadmin;
+SELECT * FROM TBEXP_EXP_SUB WHERE 1=1;
+
+select*from TBEXP_EXT_SYS_ENTRY 
+where SUBPOENA_NO='J807131004' and COST_UNIT_CODE='115000' and acct_code='64100413';
+
+select*from TBEXP_EXT_SYS_ENTRY 
+where SUBPOENA_NO='J807131001' and COST_UNIT_CODE='115000' and acct_code='65110523';
+
+select*from TBEXP_EXT_SYS_ENTRY  
+where SUBPOENA_NO='J807131002' and COST_UNIT_CODE='115000' and acct_code='65200423';
+
+select*from TBEXP_EXT_SYS_ENTRY 
+where SUBPOENA_NO='J807228005' and COST_UNIT_CODE='115000' and acct_code='64100413';
+
+select*from TBEXP_EXT_SYS_ENTRY 
+where SUBPOENA_NO='J807228003' and COST_UNIT_CODE='115000' and acct_code='65110523';
+
+select*from TBEXP_EXT_SYS_ENTRY 
+where SUBPOENA_NO='J807228002' and COST_UNIT_CODE='115000' and acct_code='65200423';
+
+select*from TBEXP_EXT_SYS_ENTRY 
+where SUBPOENA_NO='J807331006' and COST_UNIT_CODE='115000' and acct_code='64100413';
+
+select*from TBEXP_EXT_SYS_ENTRY 
+where SUBPOENA_NO='J807331003' and COST_UNIT_CODE='115000' and acct_code='65110523';
+
+select*from TBEXP_EXT_SYS_ENTRY 
+where SUBPOENA_NO='J807331004' and COST_UNIT_CODE='115000' and acct_code='65200423';
+
+--140000
+select*from TBEXP_EXT_SYS_ENTRY 
+where SUBPOENA_NO='J017331001' and COST_UNIT_CODE='140000' and acct_code='61130123' and amt='723400';
+
+select*from TBEXP_EXT_SYS_ENTRY 
+where SUBPOENA_NO='J017331001' and COST_UNIT_CODE='140000' and acct_code='61130123' and amt='481360';
+
+--114000
+select*from TBEXP_EXT_SYS_ENTRY 
+where SUBPOENA_NO='J017331001' and COST_UNIT_CODE='114000' and acct_code='61130123' and amt='253450';
+
+
+--11R000
+select*from TBEXP_EXT_SYS_ENTRY 
+where SUBPOENA_NO='J017331001' and COST_UNIT_CODE='11R000' and acct_code='61130123' and amt='214235';
+
+--12A000
+select*from TBEXP_EXT_SYS_ENTRY 
+where SUBPOENA_NO='J017331001' and COST_UNIT_CODE='12A000' and acct_code='61130123' and amt='200770';
+
+--11P000
+select*from TBEXP_EXT_SYS_ENTRY 
+where SUBPOENA_NO='J017331001' and COST_UNIT_CODE='11P000' and acct_code='61130123'  and amt='98370';
+
+select*from TBEXP_EXT_SYS_ENTRY 
+where SUBPOENA_NO='J017331001' and COST_UNIT_CODE='11P000' and acct_code='61130123' and amt='95420';
+
+--12A000
+select*from TBEXP_EXT_SYS_ENTRY 
+where SUBPOENA_NO='J017331001' and COST_UNIT_CODE='12A000' and acct_code='61130123' and amt='24900';
+
+select*from TBEXP_EXT_SYS_ENTRY 
+where SUBPOENA_NO='J017331001' and COST_UNIT_CODE='12A000' and acct_code='61130123' and amt='15250';
+
+--11P000
+select*from TBEXP_EXT_SYS_ENTRY 
+where SUBPOENA_NO='J017331001' and COST_UNIT_CODE='11P000' and acct_code='61130123' and amt='9200';
+
+--1090000
+select*from TBEXP_EXT_SYS_ENTRY 
+where SUBPOENA_NO='J017331001' and COST_UNIT_CODE='109000' and acct_code='61130123' and amt='5905';
+
+select*from TBEXP_EXT_SYS_ENTRY 
+where SUBPOENA_NO='J017331001' and COST_UNIT_CODE='109000' and acct_code='61130123' and amt='1200';
+
+select*from TBEXP_EXT_SYS_ENTRY 
+where SUBPOENA_NO='J017331001' and COST_UNIT_CODE='109000' and acct_code='61130123' and amt='400';
