4175dc56d640486fdc467f305c919d8ca169629f
twu50100 <twu50100@gmail.com>
2018-05-11 16:54:00 +0800

    Delete TF201801683_update.sql

--- TF201801683_update.sql
+++
@@ -1,3 +0,0 @@
---alter session set current_schema=expadmin;
-update TBEXP_EXT_SYS_ENTRY set PROJECT_NO='201812A300009'
-where SUBPOENA_NO='J017331001' and COST_UNIT_CODE='12A000' and acct_code='61130123' and amt='15250';
